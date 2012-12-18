36383d677c6da6eacf2843f2c4d6eeb2adafdbf3
Patrick Huesler <patrick.huesler@gmail.com>
2012-12-18 18:22:16 +0100

    Initial Commit

---
+++ qix/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  qix
+//
+//  Created by Patrick Huesler on 18.12.12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
